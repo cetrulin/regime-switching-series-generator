e48ef662520b3285110b7418072645441f182f16
cetrulin <suarezcetrulo@gmail.com>
2020-11-17 22:25:51 +0000

    changes in the selection of the series and optimizing ARMA orders

--- src/seriesSelection.py
+++ src/seriesSelection.py
@@ -36,6 +36,7 @@ config = {
     }
 
 for symbol in config['symbols']:
+    print(f'--\nSTART WITH SYMBOL: {symbol}')
     config['symbol'] = symbol
     out_path = os.sep.join([config['output_path'],
                             config['symbol'] + ('_'.join(config['years_to_explore'])) + config['extension']])
@@ -53,7 +54,7 @@ for symbol in config['symbols']:
                 if os.path.isfile(filepath):
                     print(f'Reading {file}')
                     df = pd.read_csv(filepath, names=['date', 'time', 'open', 'high', 'low', 'close', 'volume',
-                                                          'suspicious', 'Dividends', 'Extrapolation'])
+                                                      'suspicious', 'Dividends', 'Extrapolation'])
                     # print(df.head())
                     dfs.append(df)
 
@@ -77,13 +78,13 @@ for symbol in config['symbols']:
     # Parse dataframe
 
     # Generate close price returns and moving average to select the period with a mean close to 0
-    log_ret = True  # otherwise percentual
+    log_ret = False  # otherwise percentual
     if log_ret:
         concated_df['close_returns'] = np.log(concated_df['close'] / concated_df['close'].shift(1))
         # concated_df['close_returns'] = np.log(1 + concated_df['close'].pct_change())
     else:
-        # concated_df['close_returns'] = concated_df['close'] / concated_df['close'].shift(1) - 1
-        concated_df['close_returns'] = concated_df['close'].pct_change(1)
+        concated_df['close_returns'] = concated_df['close'] / concated_df['close'].shift(1) - 1
+        # concated_df['close_returns'] = concated_df['close'].pct_change(1)  # same result
 
     sma_col = f"SMA_{config['desired_length']}"
     concated_df[sma_col] = \
@@ -103,30 +104,40 @@ for symbol in config['symbols']:
     selected_df = concated_df[concated_df[sma_col].abs() <= config['desired_abs_mean_tresh']]
 
     # Export selection
-    print(f"{len(selected_df)} sets selected.\nExporting selection...")
-
+    print(f"{len(selected_df)} sets were selected.")
     for index, row in selected_df.iterrows():  # row['SMA_start_ms']) (not needed by now)
         print(f"Current selection from {row['SMA_start_date']} to { row['date']} with mean {sma_col}")
         Path(config['output_selection_path']).mkdir(parents=True, exist_ok=True)
         export_path = os.sep.join([config['output_selection_path'], config['symbol'] + row['datetime']])
-        current_selection_df = concated_df[(concated_df['SMA_start_date'].between(row['SMA_start_date'], row['date']))]
-        concated_df.sort_index(ascending=True, inplace=True)
-        current_selection_df.to_csv(export_path + config['extension'])
+        current_selection_df = concated_df[(concated_df['SMA_start_date'].between(row['SMA_start_date'],
+                                                                                  row['date']))]
+        current_selection_df.sort_index(ascending=True, inplace=True)
+        count = len(current_selection_df[current_selection_df['close_returns'] > (current_selection_df['close_returns'].std() * 3)])
 
-        # Plot set
-        current_selection_df.close.plot()
-        plt.xticks(rotation=45)
-        plt.savefig(export_path+'_close_price.png')
-        plt.show()
-        current_selection_df.close_returns.plot()
-        plt.xticks(rotation=45)
-        plt.savefig(export_path+'_returns.png')
-        plt.show()
-        current_selection_df.close_returns.hist(bins=100, alpha=0.5)
-        plt.xticks(rotation=45)
-        plt.savefig(export_path+'_returns_histogram.png')
-        plt.show()
-        plt.boxplot(current_selection_df.close_returns, label=["Close price returns"])
-        plt.xticks(rotation=45)
-        plt.savefig(export_path+'_returns_.png')
-        plt.show()
+        if (count/len(current_selection_df)*100) < 0.5:  # if the amount of outliers is less than 0.5% of the dataset we go ahead.
+            print('Exporting selection...')
+            current_selection_df.to_csv(export_path + config['extension'])
+
+            # Plot set
+            current_selection_df.close.plot()
+            plt.xticks(rotation=45)
+            plt.savefig(export_path+'_close_price.png')
+            plt.show()
+            current_selection_df.close_returns.plot()
+            plt.xticks(rotation=45)
+            plt.savefig(export_path+'_returns.png')
+            plt.show()
+            current_selection_df.close_returns.hist(bins=100, alpha=0.5)
+            plt.xticks(rotation=45)
+            plt.savefig(export_path+'_returns_histogram.png')
+            plt.show()
+            plt.boxplot(current_selection_df.close_returns, labels=["Close price returns"])
+            plt.xticks(rotation=45)
+            plt.savefig(export_path+'_returns_.png')
+            plt.show()
+        else:
+            # print(len(current_selection_df))
+            # print(current_selection_df.close_returns.head())
+            # print(current_selection_df['close_returns'].std())
+            print(f"{count} rows surpass the normal distribution. "
+                  f"This is a {np.round((count/len(current_selection_df)*100), 2)}% of the set.")
